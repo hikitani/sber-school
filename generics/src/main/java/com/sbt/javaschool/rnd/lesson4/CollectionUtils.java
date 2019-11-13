be960a70b9ce69550cecbc68ba8fc74945eae087
Kazbek <kazbekmamakaev@mail.ru>
2019-11-13 19:42:54 +0300

    add method addAll

--- generics/src/main/java/com/sbt/javaschool/rnd/lesson4/CollectionUtils.java
+++ generics/src/main/java/com/sbt/javaschool/rnd/lesson4/CollectionUtils.java
@@ -1,4 +1,11 @@
 package com.sbt.javaschool.rnd.lesson4;
 
+import java.util.ArrayList;
+import java.util.LinkedList;
+import java.util.List;
+
 public class CollectionUtils {
+    public static <T> void addAll(List<? extends T> source, List<? super T> destination) {
+        destination.addAll(source);
+    }
 }
