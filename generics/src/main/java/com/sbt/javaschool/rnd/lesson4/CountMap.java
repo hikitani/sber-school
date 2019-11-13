a3c2c669d2d19805ecaf7cc35f4d45e0156399ae
Kazbek <kazbekmamakaev@mail.ru>
2019-11-13 18:27:06 +0300

    change interface CountMap

--- generics/src/main/java/com/sbt/javaschool/rnd/lesson4/CountMap.java
+++ generics/src/main/java/com/sbt/javaschool/rnd/lesson4/CountMap.java
@@ -15,12 +15,12 @@ public interface CountMap<T> {
     //Количество разных элементов
     int size();
 
-    //Добавить все элементы из source в текущий контейнер, при совпадении ключей,     суммировать значения
-    void addAll(CountMap source);
+    //Добавить все элементы из source в текущий контейнер, при совпадении ключей, суммировать значения
+    void addAll(CountMap<T> source);
 
     //Вернуть java.util.Map. ключ - добавленный элемент, значение - количество его добавлений
-    Map toMap();
+    Map<T, Integer> toMap();
 
     //Тот же самый контракт как и toMap(), только всю информацию записать в destination
-    void toMap(Map destination);
+    void toMap(Map<T, Integer> destination);
 }
